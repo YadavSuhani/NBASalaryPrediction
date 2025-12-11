1195aa7c5b299dd0552a5cc2a54f0d3135c08a15
aaryamikg <gupta848@purdue.edu>
2025-12-11 14:26:52 -0500

    rerun XG

--- XGboost.py
+++ XGboost.py
@@ -20,6 +20,8 @@ FEATURE_COLS = [
     "assists_s1", "assists_s2",
     "rebounds_s1", "rebounds_s2",
     #"steals_s1", "steals_s2",
+    "RPM_s1","RPM_s2",
+    "USG_s1","USG_s2",
     "3pts_made_s1", "3pts_made_s2",
     "3pts_attempted_s1", "3pts_attempted_s2",
     "free_throws_made_s1", "free_throws_made_s2",
